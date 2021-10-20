573528acedc2e1609bbffaa153b2e683aa0f025a
Paweł <01142223@pw.edu.pl>
2021-10-21 01:17:44 +0200

    Kopia zapasowa na koniec dnia

--- src/fcm_creation.py
+++ src/fcm_creation.py
@@ -2,63 +2,43 @@
 """
 import numpy as np
 
-from geneticalgorithm2 import geneticalgorithm2 as ga # for creating and running optimization model
+def sigmoid(x):
+    return 1 / (1 + np.exp(-5*x))
 
-from geneticalgorithm2 import Generation, AlgorithmParams # classes for comfortable parameters setting and getting
+def g_func(x):
+    return sigmoid(x)
 
-from geneticalgorithm2 import Crossover, Mutations, Selection # classes for specific mutation and crossover behavior
-
-from geneticalgorithm2 import Population_initializer # for creating better start population
-
-from geneticalgorithm2 import np_lru_cache # for cache function (if u want)
-
-from geneticalgorithm2 import plot_pop_scores # for plotting population scores, if u want
-
-from geneticalgorithm2 import Callbacks # simple callbacks
-
-from geneticalgorithm2 import Actions, ActionConditions, MiddleCallbacks # middle callbacks
+#previous_considered_indices na ten moment musi być np.ndarray bo series[i - ...]
+def create_fitness_func(series, previous_considered_indices, space_dim):
+    w_matrix_offset = previous_considered_indices.size * space_dim
+    max_previous_index = previous_considered_indices.max()
+    def fitness_func(trained_array):
+        u_matrix = trained_array[:w_matrix_offset].reshape(-1, space_dim)
+        w_matrix = trained_array[w_matrix_offset:].reshape(-1, space_dim)
+        inputs = np.array(map(lambda val: val - ))
+        #Nasze targety mają indeksy z range(max_previous_index, series.shape[0])
+        #Tworzę tablicę zawierającą dla każdego tego elementu listę indeksów
+        #jego punktów szeregów wejciowych
+        input_indices = (
+            np.arange(max_previous_index, series.shape([0]))
+                [:, np.newaxis].repeat(previous_considered_indices.size, axis = 1)
+                - previous_considered_indices
+            )
+        def compute_a_values(inputs):
+            return (u_matrix * inputs).sum(axis = 0)
+        a_matrix = np.apply_along_axis(compute_a_values, 1, series[input_indices])
+        #Wartosci w a sa poukladane wierszowo, czyli sa 3 kolumny z a na kazdej wspolrzednej
+        #TODO niżej transpozycje wyciagnac na zewnątrz
+        y_matrix = np.matmul(w_matrix, a_matrix.transpose()).transpose()
+        #Teraz w każdym wierszu mamy odpowiadające wyjcie z FCMa
+        target_matrix = series[np.arange(max_previous_index, series.shape([0]))]
+        sse = (y_matrix - target_matrix)**2
+        return sse
+    return fitness_func
+            
 
 def create_fcm(series, previous_considered_indices):
     space_dim = series.shape[1]
     u_matrix_bounds = np.array([[-1, 1]] * (previous_considered_indices.size * space_dim))
     w_matrix_bounds = np.array([[-1, 1]] * space_dim**2)
     all_bounds = np.vstack(u_matrix_bounds, w_matrix_bounds)
-
-test = np.array([[0, 10]] * 3**2)
-def function(x):
-    return np.sum(x)
-model = ga(function, dimension = 9, 
-                variable_type='real', 
-                 variable_boundaries = test,
-                 variable_type_mixed = None, 
-                 function_timeout = 10,
-                 algorithm_parameters=AlgorithmParams()
-            )   
-model.run(
-    no_plot = False, 
-    disable_progress_bar = False,
-    disable_printing = False,
-
-    set_function = None, 
-    apply_function_to_parents = False, 
-    start_generation = {'variables':None, 'scores': None},
-    studEA = False,
-    mutation_indexes = None,
-
-    init_creator = None,
-    init_oppositors = None,
-    duplicates_oppositor = None,
-    remove_duplicates_generation_step = None,
-    revolution_oppositor = None,
-    revolution_after_stagnation_step = None,
-    revolution_part = 0.3,
-    
-    population_initializer = Population_initializer(select_best_of = 1, local_optimization_step = 'never', local_optimizer = None),
-    
-    stop_when_reached = None,
-    callbacks = [],
-    middle_callbacks = [],
-    time_limit_secs = None, 
-    save_last_generation_as = None,
-    seed = None
-    )
