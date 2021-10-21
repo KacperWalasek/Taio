8eabcc9f104f4fab4578e64a8b30ef90a4fab281
Paweł <01142223@pw.edu.pl>
2021-10-21 12:55:11 +0200

    Gotowa funkcja fitnesu - testowałem ręcznie linijka po linijce
    Potem okomentuję kod - na razie tylko robocze komentarze dla siebie

--- src/fcm_creation.py
+++ src/fcm_creation.py
@@ -9,36 +9,59 @@ def g_func(x):
     return sigmoid(x)
 
 #previous_considered_indices na ten moment musi być np.ndarray bo series[i - ...]
-def create_fitness_func(series, previous_considered_indices, space_dim):
+def _create_fitness_func(series, previous_considered_indices, space_dim):
     w_matrix_offset = previous_considered_indices.size * space_dim
     max_previous_index = previous_considered_indices.max()
     def fitness_func(trained_array):
         u_matrix = trained_array[:w_matrix_offset].reshape(-1, space_dim)
         w_matrix = trained_array[w_matrix_offset:].reshape(-1, space_dim)
-        inputs = np.array(map(lambda val: val - ))
         #Nasze targety mają indeksy z range(max_previous_index, series.shape[0])
         #Tworzę tablicę zawierającą dla każdego tego elementu listę indeksów
         #jego punktów szeregów wejciowych
         input_indices = (
-            np.arange(max_previous_index, series.shape([0]))
+            np.arange(max_previous_index, series.shape[0])
                 [:, np.newaxis].repeat(previous_considered_indices.size, axis = 1)
                 - previous_considered_indices
-            )
-        def compute_a_values(inputs):
-            return (u_matrix * inputs).sum(axis = 0)
-        a_matrix = np.apply_along_axis(compute_a_values, 1, series[input_indices])
+            )      
+        a_matrix = (series[input_indices]*u_matrix).sum(axis = 1)
+
         #Wartosci w a sa poukladane wierszowo, czyli sa 3 kolumny z a na kazdej wspolrzednej
-        #TODO niżej transpozycje wyciagnac na zewnątrz
-        y_matrix = np.matmul(w_matrix, a_matrix.transpose()).transpose()
+        y_matrix = np.matmul(a_matrix, w_matrix.T)
         #Teraz w każdym wierszu mamy odpowiadające wyjcie z FCMa
-        target_matrix = series[np.arange(max_previous_index, series.shape([0]))]
-        sse = (y_matrix - target_matrix)**2
+        target_matrix = series[np.arange(max_previous_index, series.shape[0])]
+        sse = ((y_matrix - target_matrix)**2).sum()
         return sse
     return fitness_func
             
 
 def create_fcm(series, previous_considered_indices):
     space_dim = series.shape[1]
-    u_matrix_bounds = np.array([[-1, 1]] * (previous_considered_indices.size * space_dim))
-    w_matrix_bounds = np.array([[-1, 1]] * space_dim**2)
-    all_bounds = np.vstack(u_matrix_bounds, w_matrix_bounds)
+    trained_array_size = previous_considered_indices.size * space_dim + space_dim**2
+    trained_array_bounds = np.array([[-1, 1]] * trained_array_size)
+    fitness_func = _create_fitness_func(series, previous_considered_indices, space_dim)
+    model = ga(fitness_func, dimension = trained_array_size, 
+                variable_type='real', 
+                 variable_boundaries = trained_array_bounds,
+                 variable_type_mixed = None, 
+                 function_timeout = 10,
+                 algorithm_parameters=AlgorithmParams(
+                     max_num_iteration = None,
+                     population_size = 100,
+                     mutation_probability = 0.1,
+                     elit_ratio = 0.01,
+                     crossover_probability = 0.5,
+                     parents_portion = 0.3,
+                     crossover_type = 'uniform',
+                     mutation_type = 'uniform_by_center',
+                     selection_type = 'roulette',
+                     max_iteration_without_improv = None
+                     )
+            )
+    model = ga(fitness_func, dimension = trained_array_size, 
+                variable_type='real', 
+                 variable_boundaries = trained_array_bounds,
+                 variable_type_mixed = None, 
+                 function_timeout = 10,
+                 algorithm_parameters=AlgorithmParams()
+            )
+    model.run()
