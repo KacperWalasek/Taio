20d8295b5d5c14c27e90d17a32f938f1faf5b994
Paweł <01142223@pw.edu.pl>
2021-10-25 14:58:40 +0200

    poprawki

--- src/series_classification.py
+++ src/series_classification.py
@@ -5,21 +5,22 @@ import os
 import datetime
 import functools
 from multiprocessing import Pool
+import sys
 import numpy as np
 import read_data
 import fcm_creation
 import svm
-import sys
+
 
 def _process_file(file_info, prev):
     file_path = file_info["path"]
     series = read_data.process_data(file_path)
     fcm = fcm_creation.create_fcm(series, prev)
-    print(f"Processing {file_info['name']} from class {file_info['class']} ended\n")
+    # print(f"Processing {file_info['name']} from class {file_info['class']} ended\n")
     return file_info["class"], fcm
 
 
-def create_models(dir):
+def create_models(dir_path):
     """
     Creates fcm models out of files in given directory
 
@@ -34,7 +35,7 @@ def create_models(dir):
     file_infos = []
     print(datetime.datetime.now())
 
-    for class_dir in (entry for entry in os.scandir(dir) if entry.is_dir()):
+    for class_dir in (entry for entry in os.scandir(dir_path) if entry.is_dir()):
         for file in os.scandir(class_dir.path):
             if file.name.endswith(".csv"):
                 file_infos.append(
@@ -47,30 +48,55 @@ def create_models(dir):
     return zip(*svm_training_data)
 
 
-def train(dir):
-    svm_classes, svm_fcms = create_models(dir)
+def train(dir_path):
+    """
+
+
+    Parameters
+    ----------
+    dir : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    classes, fcms = create_models(dir_path)
     svm.save_svm(
-        svm.create_and_train_svm(np.array(svm_fcms), np.array(svm_classes), kernel="rbf"),
+        svm.create_and_train_svm(np.array(fcms), np.array(classes), kernel="rbf"),
         "./svmFile.joblib",
     )
 
 
-def test(dir):
-    svm_classes, svm_fcms = create_models(dir)
-    classes = svm.classify(svm.load_svm("./svmFile.joblib"),np.array(svm_fcms))
+def test(dir_path):
+    """
+
+
+    Parameters
+    ----------
+    dir : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+
+    """
+    classes, fcms = create_models(dir_path)
+    predicted_classes = svm.classify(svm.load_svm("./svmFile.joblib"), np.array(fcms))
     correct_ones = 0
-    for i in range(len(classes)):
-        if classes[i] == svm_classes[i]:
+    for predicted_class_number, class_number in zip(predicted_classes, classes):
+        if predicted_class_number == class_number:
             correct_ones += 1
-    print("Classification result: ",correct_ones/len(classes))
+    print("Classification result: ", correct_ones / len(classes))
 
 
 if __name__ == "__main__":
-    if len(sys.argv)!=2 or ( sys.argv[1]!='train' and sys.argv[1]!='test' ):
-        print('\'train\' or \'test\' argument is needed')
+    if len(sys.argv) != 2 or (sys.argv[1] != "train" and sys.argv[1] != "test"):
+        print("'train' or 'test' argument is needed")
     else:
-        if sys.argv[1]=='train':
+        if sys.argv[1] == "train":
             train(os.path.join("UWaveGestureLibrary_Preprocessed", "Train"))
         else:
             test(os.path.join("UWaveGestureLibrary_Preprocessed", "Test"))
-
