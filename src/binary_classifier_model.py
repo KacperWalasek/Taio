2426ff695d75c3a08b7258a24ebae60b729ee734
Paweł <wesolowski.p@protonmail.com>
2022-02-15 10:09:26 +0100

    jest przyspieszenie 2-3x na moim kompie jesli chodzi o czas fitnesu

--- src/binary_classifier_model.py
+++ src/binary_classifier_model.py
@@ -3,11 +3,10 @@ BinaryClassifierModel model (2 classification classes).
 """
 
 import numpy as np
-from pyfde import ClassicDE
 from geneticalgorithm2 import geneticalgorithm2 as ga
 from geneticalgorithm2 import AlgorithmParams
-import utils
 import functools
+import computing_utils
 
 
 class BinaryClassifierModel:
@@ -68,94 +67,10 @@ class BinaryClassifierModel:
         self._uwv_matrices = []
         self.is_trained = False
 
-    def _predict_series(self, membership_matrix, u_matrix, w_matrix, v_matrix):
-
-        max_previous_index = self._previous_considered_indices.max()
-
-        # Note that targets for which we calculate predictions are
-        # membership_matrix[max_previous_index:membership_matrix.shape[0]].
-        # For every predicion we create an array containing indices
-        # of its input elements.
-        input_indices = (
-            np.arange(max_previous_index, membership_matrix.shape[0], self._move)[
-                :, np.newaxis
-            ].repeat(self._previous_considered_indices.size, axis=1)
-            - self._previous_considered_indices
-        )
-
-        if input_indices.size == 0:
-            raise RuntimeError(
-                "Specified previous_considered_indices array invalid for given dataset"
-                " - one of the series after prepocessing with read_data module"
-                f" has {membership_matrix.shape[0] + 1} elements."
-                " Remember that c-means processed series has one element less"
-                " due to adding adjacent differences to points (the first one is discarded)."
-            )
-
-        # Now we compute matrix containing a-values for each target.
-        # The number of columns is equal to concept_count (each target element
-        # corresponds to single row which contains a-value for each concept).
-        a_matrix = self._sigmoid(
-            (membership_matrix[input_indices] * u_matrix).sum(axis=1)
-        )
-
-        # We calculate predicted values for each target.
-        predicted_concept_membership_matrix = self._sigmoid(
-            np.matmul(a_matrix, w_matrix.T)
-        )
-
-        # The last step - class prediction.
-        # v_matrix must be of shape (2, concept_count)
-        predicted_class_memberships = self._sigmoid(
-            (predicted_concept_membership_matrix[:, np.newaxis] * v_matrix).sum(axis=2)
-        )
-        discrete_class_memberships = np.zeros_like(
-            predicted_class_memberships, dtype=np.int32
-        )
-        # Below range is necessary.
-        discrete_class_memberships[
-            range(predicted_class_memberships.shape[0]),
-            predicted_class_memberships.argmax(1),
-        ] = 1
-        total_votes = discrete_class_memberships.sum(axis=0)
-        window_count = total_votes.sum()
-        total_weights = predicted_class_memberships.sum(axis=0) / window_count
-        chosen_class_idx = (
-            total_votes.argmax()
-            if total_votes[0] != total_votes[1]
-            else total_weights.argmax()
-        )
-        return self.class_numbers[chosen_class_idx], total_weights
-
-
-    def _create_new_fitness_func(self):
-        stacked_matrices = [np.vstack(x) for x in self._membership_matrices]
-        series_lengths = list(map(lambda x: np.array([y.shape[0] for y in x]), self._membership_matrices))
-        def fitness_func(solution):
-            u_matrix, w_matrix, v_matrix = self._split_uwv_array(solution)
-            result = 0
-            for idx in range(2):
-                result += np.sum(utils._predict_all_series(self._previous_considered_indices, self._move, stacked_matrices[idx], series_lengths[idx], u_matrix, w_matrix, v_matrix) != idx)
-            return result
-        return fitness_func
-
     def _create_fitness_func(self):
-        # def fitness_func(solution):
-        #     solution = np.array(solution)
-        #     u_matrix, w_matrix, v_matrix = self._split_uwv_array(solution)
-        #     misclassified_count = 0
-        #     for idx, class_number in enumerate(self.class_numbers):
-        #         for membership_matrix in self._membership_matrices[idx]:
-        #             assigned_class_number = self._predict_series(
-        #                 membership_matrix, u_matrix, w_matrix, v_matrix
-        #             )[0]
-        #             if assigned_class_number != class_number:
-        #                 misclassified_count += 1
-        #     return misclassified_count
-        result = functools.partial(utils.fitness_func, membership_matrices = self._membership_matrices, 
+        return functools.partial(computing_utils.fitness_func, membership_matrices = self._membership_matrices, 
                 previous_considered_indices = self._previous_considered_indices,
                 move = self._move)
-        return result
 
     def train(self):
         """
@@ -168,7 +83,7 @@ class BinaryClassifierModel:
         """
         if self.is_trained:
             return self
-        fitness_func = self._create_fitness_func()#_create_fitness_func()
+        fitness_func = self._create_fitness_func()
         trained_array_size = (
             self._previous_considered_indices.size * self._concept_count
             + self._concept_count ** 2
@@ -176,12 +91,13 @@ class BinaryClassifierModel:
         )
         bounds = np.array([[-1, 1]] * trained_array_size)
         ga_model = ga(
-            fitness_func,
+            function = fitness_func,
             dimension=trained_array_size,
             variable_boundaries=bounds,
             **self._GA_PARAMS,
         )
         ga_model.run(
+            set_function=ga.set_function_multiprocess(fitness_func),
             stop_when_reached=0,
             **self._GA_RUN_PARAMS,
         )
@@ -193,15 +109,7 @@ class BinaryClassifierModel:
             flush=True,
         )
 
-        # solver = ClassicDE(fitness_func, n_dim = trained_array_size, n_pop = 800, limits = (-1., 1.))
-        # index = 0
-        # for best, fit in solver(n_it=1):
-        #     print(f"{index}, fit: {fit}")
-        #     index += 1
-        # print(f"Best fit {fit}")
-
-        self._uwv_matrices = self._split_uwv_array(solution)
-        # self._uwv_matrices = self._split_uwv_array(np.array(best))
+        self._uwv_matrices = computing_utils.split_uwv_array(solution, self._previous_considered_indices.size, self._concept_count)
         self.is_trained = True
         del self._membership_matrices
         return self
@@ -231,7 +139,8 @@ class BinaryClassifierModel:
         if not self.is_trained:
             raise RuntimeError("Classifier has not been trained")
         # pylint: disable = no-value-for-parameter
-        return self._predict_series(membership_matrix, *self._uwv_matrices)
+        prediction = computing_utils.predict_series_class_idx(membership_matrix, *self._uwv_matrices, self._previous_considered_indices, self._move)
+        return self.class_numbers[prediction[0]], prediction[1]
 
 
 if __name__ == "__main__":
@@ -252,20 +161,12 @@ if __name__ == "__main__":
         (5, 6), test_membership_matrices, None, test_previous_indices, 2
     )
 
-    test1 = np.array([test_membership_1, test_membership_2])
-    test2 = np.array([test_membership_3])
-
-    model2 = BinaryClassifierModel(
-        (5, 6), (test1, test2), None, test_previous_indices, 2 
-    )
-    model2._predict_all_series(np.vstack([test_membership_1, test_membership_2]), np.r_[test_membership_1.shape[0], test_membership_2.shape[0]], np.arange(3 * 3).reshape(3, -1), np.arange(3*3).reshape(3, -1), np.arange(3*2).reshape(2, -1))
-
-    # model.train()
-    # if (
-    #     model.predict(test_membership_1)[0] == 5
-    #     and model.predict(test_membership_2)[0] == 5
-    #     and model.predict(test_membership_3)[0] == 6
-    # ):
-    #     print("OK")
-    # else:
-    #     print("NOT OK")
+    model.train()
+    if (
+        model.predict(test_membership_1)[0] == 5
+        and model.predict(test_membership_2)[0] == 5
+        and model.predict(test_membership_3)[0] == 6
+    ):
+        print("OK")
+    else:
+        print("NOT OK")
---
+++ src/computing_utils.py
@@ -0,0 +1,101 @@
+import numba as nb
+import numpy as np
+
+
+@nb.njit(cache=True)
+def sigmoid(x):
+    for i in range(x.shape[0]):
+        x[i] = 1 / (1 + np.exp(-5 * x[i]))
+    return x
+
+
+@nb.njit(cache=True)
+def split_uwv_array(array, previous_considered_indices_size, concept_count):
+    w_matrix_offset = previous_considered_indices_size * concept_count
+    v_matrix_offset = w_matrix_offset + concept_count ** 2
+    u_matrix = array[:w_matrix_offset].reshape(-1, concept_count)
+    w_matrix = array[w_matrix_offset:v_matrix_offset].reshape(-1, concept_count)
+    v_matrix = array[v_matrix_offset:].reshape(2, -1)
+    return u_matrix, w_matrix, v_matrix
+
+
+@nb.njit(cache=True)
+def predict_series_class_idx(
+    membership_matrix, u_matrix, w_matrix, v_matrix, previous_considered_indices, move
+):
+    max_previous_index = previous_considered_indices.max()
+
+    total_votes = np.zeros(2, dtype=np.int32)
+    total_weights = np.zeros(2)
+
+    concept_count = membership_matrix.shape[1]
+
+    # It is important that these arrays are allocated only once.
+    a_array = np.empty(concept_count)
+    predicted_concept_membership_array = np.empty(concept_count)
+    predicted_class_memberships = np.empty(2)
+
+    for i in range(max_previous_index, membership_matrix.shape[0], move):
+
+        for j in range(concept_count):
+            a_array[j] = 0
+            for k in range(previous_considered_indices.size):
+                a_array[j] += (
+                    membership_matrix[i - previous_considered_indices[k], j]
+                    * u_matrix[k, j]
+                )
+        a_array = sigmoid(a_array)
+
+        for j in range(concept_count):
+            predicted_concept_membership_array[j] = 0
+            for k in range(concept_count):
+                predicted_concept_membership_array[j] += w_matrix[j, k] * a_array[k]
+        predicted_concept_membership_array = sigmoid(
+            predicted_concept_membership_array
+        )
+
+        for j in range(2):
+            predicted_class_memberships[j] = 0
+            for k in range(concept_count):
+                predicted_class_memberships[j] += (
+                    v_matrix[j, k] * predicted_concept_membership_array[k]
+                )
+        predicted_class_memberships = sigmoid(predicted_class_memberships)
+
+        if predicted_class_memberships[0] >= predicted_class_memberships[1]:
+            total_votes[0] += 1
+        else:
+            total_votes[1] += 1
+        for j in range(2):
+            total_weights[j] += predicted_class_memberships[j]
+
+    if total_votes[0] < total_votes[1]:
+        selected_idx = 1
+    elif total_votes[0] > total_votes[1]:
+        selected_idx = 0
+    elif total_weights[0] < total_weights[1]:
+        selected_idx = 1
+    else:
+        selected_idx = 0
+    return selected_idx, total_weights
+
+@nb.njit(cache=True)
+def fitness_func(solution, membership_matrices, previous_considered_indices, move):
+
+    u_matrix, w_matrix, v_matrix = split_uwv_array(
+        solution, previous_considered_indices.size, membership_matrices[0][0].shape[1]
+    )
+    misclassified_count = 0
+    for idx in range(2):
+        for membership_matrix in membership_matrices[idx]:
+            assigned_class_idx = predict_series_class_idx(
+                membership_matrix,
+                u_matrix,
+                w_matrix,
+                v_matrix,
+                previous_considered_indices,
+                move,
+            )[0]
+            if assigned_class_idx != idx:
+                misclassified_count += 1
+    return misclassified_count
