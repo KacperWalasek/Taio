d5620eabb2c4247499fe0183c161da7978a137ea
Władysław Homenda <whomenda@eden@mini.pw.edu.pl>
2022-06-09 01:03:29 +0200

    zmiany formatera

--- src/tools/sbatch_formatter.py
+++ src/tools/sbatch_formatter.py
@@ -1,8 +1,12 @@
 """
 Script for creating sbatch files for SLURM.
 """
-
+import sys
 import os
+if len(sys.argv) < 2:
+    raise RuntimeError("Too few arguments, virtual environment name is missing")
+else:
+    envName = sys.argv[1]
 
 with open("start_template", "r", encoding='UTF-8') as file:
     template = file.read()
@@ -21,7 +25,7 @@ for series_data_dir in os.scandir("tests"):
         else:
             CPUS = 1
         formatted_template = template.format(
-            cpus=CPUS, name=series_data_dir.name
+            cpus=CPUS, name=series_data_dir.name, envName=envName
         )
         with open(f"start_{series_data_dir.name}", "w", encoding='UTF-8') as file:
             file.write(formatted_template)
